08f512facf1952ece78d1b2d304709ae9177d8a5
Eduardo Brito <alu0100783315@ull.edu.es>
2021-04-25 18:29:09 +0100

    Deleted empty constructor as per SonarCloud

--- src/notes.ts
+++ src/notes.ts
@@ -17,11 +17,6 @@ export class Notes {
   private static notesInstance: Notes;
 
   /**
-   * @description Notes class constructor
-   */
-  constructor() { }
-
-  /**
    * @description Static method that returns the notes instance. If it doesn't
    * exist already, it creates it.
    * @returns The static Notes instance
