c517896c29204b107b2c225e58f644894df10418
Eduardo Brito <alu0100783315@ull.edu.es>
2021-04-25 17:35:46 +0100

    Static instance method test passing

--- src/notes.ts
+++ src/notes.ts
@@ -2,5 +2,22 @@
  * @description Notes class that defines several filesystem methods.
  */
 export class Notes {
-  
+  private static notesInstance: Notes;
+
+  /**
+   * @description Notes class constructor
+   */
+  constructor() { };
+
+  /**
+   * @description Static method that returns the notes instance. If it doesn't
+   * exist already, it creates it.
+   * @returns The static Notes instance
+   */
+  public static getNotesInstance(): Notes {
+    if (!Notes.notesInstance) {
+      Notes.notesInstance = new Notes();
+    }
+    return Notes.notesInstance;
+  }
 }
