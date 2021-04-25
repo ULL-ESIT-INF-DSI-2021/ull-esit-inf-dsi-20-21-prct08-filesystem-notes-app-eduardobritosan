0d138e9837a9bd145808eceed6f444e34bd8f9d2
Eduardo Brito <alu0100783315@ull.edu.es>
2021-04-25 17:50:52 +0100

    Add note test failing

--- tests/notes.spec.ts
+++ tests/notes.spec.ts
@@ -4,12 +4,22 @@ import { Notes } from '../src/notes';
 
 
 describe('Notes methods tests', () => {
-  describe('Notes getter function tests', () => {
+  describe('Notes instance getter function tests', () => {
     it('Notes.getNotesInstance() should be an object', () => {
       expect(Notes.getNotesInstance()).to.be.an('object');
     });
     it('Notes instance should be already initialized', () => {
       expect(Notes.getNotesInstance()).to.be.an('object');
+    });
+  });
+
+  const noteInstance = Notes.getNotesInstance();
+
+  describe('Adding a new note methods tests', () => {
+    it('addNote should return \'New note added!\'', () => {
+      expect(noteInstance.addNote('brito', 'Red note',
+        'Content of the red note, by brito',
+        'Red')).to.equal('New note added!');
     });
   });
 });
