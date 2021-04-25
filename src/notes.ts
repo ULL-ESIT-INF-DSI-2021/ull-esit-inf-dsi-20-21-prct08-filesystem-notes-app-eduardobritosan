aca15a5550979cfdc7ec6fd8cd15c2edd9cf2741
Eduardo Brito <alu0100783315@ull.edu.es>
2021-04-25 18:04:26 +0100

    Add note test passing

--- src/notes.ts
+++ src/notes.ts
@@ -1,3 +1,5 @@
+import * as Files from 'fs';
+
 /**
  * @description Notes class that defines several filesystem methods.
  */
@@ -7,7 +9,7 @@ export class Notes {
   /**
    * @description Notes class constructor
    */
-  constructor() {}
+  constructor() { }
 
   /**
    * @description Static method that returns the notes instance. If it doesn't
@@ -19,5 +21,35 @@ export class Notes {
       Notes.notesInstance = new Notes();
     }
     return Notes.notesInstance;
+  }
+
+  /**
+   * @description Method that adds new notes and saves them in the filesystem
+   * @param user The owner of the note's username
+   * @param title The title of the new note
+   * @param content The content of the note
+   * @param color The color of the note
+   * @returns a string saying 'New note added!'
+   */
+  public addNote(user: string, title: string, content: string, color: string) {
+    const dataString =
+      `{ "title": "${title}", "content": "${content}", "color": "${color}" }`;
+    const folder = this.getRoute(user);
+    const route = folder + title;
+    Files.writeFileSync(route, dataString);
+    return 'New note added!';
+  }
+
+  /**
+   * Returns the route of the folder that stores a user's notes
+   * @param user The name of the user from which the folder is found
+   * @returns The route of the folder
+   */
+  private getRoute(user: string) {
+    const route = `./src/${user}/`;
+    if (!Files.existsSync(route)) {
+      Files.mkdirSync(route);
+    }
+    return route;
   }
 }
